f6a27ef2fa14a3bf0cf520b675f394fe79d27ae9
Martyna <martyna.b@hotmail.com>
2019-12-13 16:13:13 -0800

    cleaning the code and adding comments

--- index.js
+++ index.js
@@ -8,91 +8,14 @@ const apiKey = '200650145-102d40953dcd593ca8cd47bf5ba24ff1';
 const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
 const appId = 'ae76d0efed32d9f29c4d54a5738b80ca';
 
-function getWeather(responseJson) {
-  const lat = responseJson.results[0].geometry.lat;
-  const lng = responseJson.results[0].geometry.lng;
-  const city = responseJson.results[0].components.city;
-
-  const params = {   
-    city: city,
-    lat: lat,
-    lon: lng,
-    appId: appId,
-    units: 'imperial'
-        };
-    
-  const queryString = formatQueryParams(params)
-  const url = weatherUrl + '?' + queryString;
-    
-  console.log(url, {mode: 'no-cors'});
-
-   fetch(url)
-   .then(response => {
-    if (response.ok) {
-      return response.json();
-    }
-    throw new Error(response.statusText);
-  })
-  .then(responseJson_3 => {
-    displayWeather(responseJson_3);
-    })
-  .catch(err => {
-    console.log(err.message);
-    //hide previous results and display 404 Not found
-    $('#results').addClass('hidden')
-    $('#js-error-message').append(
-      `<li id='js-result_2'>
-      <div><img src="NotFound.png" class="result-img"></div>
-      </li>`
-    )
-  });
-};
-
+//function to format search params
 function formatQueryParams(params) {
   const queryItems = Object.keys(params)
     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
   return queryItems.join('&');
-}
+};
 
-function getTrails(responseJson) {
-  console.log(responseJson);
-
-  const lat = responseJson.results[0].geometry.lat;
-  const lng = responseJson.results[0].geometry.lng;
-  console.log(lat,lng);
-
-  const params = {   
-    lat: lat,
-    lon: lng,
-    maxDistance: 15,
-    key: apiKey,
-    };
-
-  const queryString = formatQueryParams(params)
-  const url = searchURL + '?' + queryString;
-
-  console.log(url, {mode: 'no-cors'});
-
-  fetch(url)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error(response.statusText);
-    })
-    .then(responseJson_2 => displayResults(responseJson_2))
-    .catch(err => {
-      console.log(err.message);
-      //hide previous results and display 404 Not found
-      $('#results').addClass('hidden')
-      $('#js-error-message').append(
-        `<li id='js-result_2'>
-      <div><img src="NotFound.png" class="result-img"></div>
-      </li>`
-      );
-    });
-}
-
+//pass the search term and call in reverse geo API to get lat&lng
 function getGeo(searchTerm) {
     const params = {
         key: geoKey,
@@ -102,7 +25,7 @@ function getGeo(searchTerm) {
       };
 
     const queryString = formatQueryParams(params)
-    const url = geoURL + '?' + queryString;
+    const url = `${geoURL}?${queryString}`;
     
     console.log(url);
     
@@ -122,14 +45,96 @@ function getGeo(searchTerm) {
         //hide previous results and display Not found
         $('#results').addClass('hidden')
         $('#js-error-message').append(
-          `<li id='js-result_2'>
-      <div><img src="NotFound.png" class="result-img"></div>
-      </li>`
+          `<li id="js-result_2">
+          <div><img src="NotFound.png" class="result-img"></div>
+          </li>`
         );
     });
 
 };
 
+//pass the lat&lng to get the waether info on the searched location
+function getWeather(responseJson) {
+  const lat = responseJson.results[0].geometry.lat;
+  const lng = responseJson.results[0].geometry.lng;
+  const city = responseJson.results[0].components.city;
+
+  const params = {   
+    city: city,
+    lat: lat,
+    lon: lng,
+    appId: appId,
+    units: 'imperial'
+        };
+    
+  const queryString = formatQueryParams(params)
+  const url = `${weatherUrl}?${queryString}`;
+    
+  console.log(url);
+
+  fetch(url)
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error(response.statusText);
+  })
+  .then(responseJson_3 => {
+    displayWeather(responseJson_3);
+    })
+  .catch(err => {
+    console.log(err.message);
+    //hide previous results and display 404 Not found
+    $('#results').addClass('hidden')
+    $('#js-error-message').append(
+      `<li id="js-result_2">
+      <div><img src="NotFound.png" class="result-img"></div>
+      </li>`
+    )
+  });
+};
+
+//pass the lat&lng to get the info on the trails in the area
+function getTrails(responseJson) {
+  console.log(responseJson);
+
+  const lat = responseJson.results[0].geometry.lat;
+  const lng = responseJson.results[0].geometry.lng;
+  console.log(lat,lng);
+
+  const params = {   
+    lat: lat,
+    lon: lng,
+    maxDistance: 15, //limit the result to 15miles
+    key: apiKey,
+    };
+
+  const queryString = formatQueryParams(params)
+  const url = `${searchURL}?${queryString}`;
+
+  console.log(url);
+
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson_2 => displayResults(responseJson_2))
+    .catch(err => {
+      console.log(err.message);
+      //hide previous results and display if Not found
+      $('#results').addClass('hidden')
+      $('#js-error-message').append(
+        `<li id="js-result_2">
+      <div><img src="NotFound.png" class="result-img"></div>
+      </li>`
+      );
+    });
+};
+
+//display the data for the weather
 function displayWeather(responseJson_3) {
     console.log(responseJson_3);
 
@@ -137,11 +142,11 @@ function displayWeather(responseJson_3) {
     $('#js-error-message').empty();
     $('#js-weather').empty();
 
-    const city = responseJson_3.name;
-    const temp = Math.floor(responseJson_3.main.temp) + "°" + "F";
-    const humidity = 'at ' + responseJson_3.main.humidity + '%';
-    const summary = responseJson_3.weather[0].description;
-    const weatherIcon = "https://api.openweathermap.org/img/w/" + responseJson_3.weather[0].icon + ".png";
+    const city = `${responseJson_3.name}`;
+    const temp = `${Math.floor(responseJson_3.main.temp)}° F`;
+    const humidity = `at ${responseJson_3.main.humidity}%`;
+    const summary = `${responseJson_3.weather[0].description}`;
+    const weatherIcon = `https://api.openweathermap.org/img/w/${responseJson_3.weather[0].icon}.png`;
 
     
     $('#js-weather').append(
@@ -152,8 +157,9 @@ function displayWeather(responseJson_3) {
         <p>Humidity<span> ${humidity}</span></p>
         </li>`
     ); 
-}
+};
 
+//display the data for the trails
 function displayResults(responseJson_2) {
     console.log(responseJson_2);
     const trails = responseJson_2.trails;
@@ -162,17 +168,17 @@ function displayResults(responseJson_2) {
     $('#js-error-message').empty();
     $('#results-list').empty();
   
-    //throw 404 if result not found
+    //throw Not Found if result not found
     if (trails.length === 0) {
       $('#results-list').append(
-        `<li id='js-result_2'>
+        `<li id="js-result_2">
         <div class="image-wrap"><img src="NotFound.png" class="result-img"></div>
         </li>`
       )
     } else {
       // iterate through the length of the repos array
       for (let i = 0; i < trails.length; i++){
-        // add a list item to the results 
+      // add a list of items to the results 
       //list parks info
       $('#results-list').append(
         `<li><h3 id="js-name">${trails[i].name}</h3>
@@ -192,14 +198,13 @@ function displayResults(responseJson_2) {
   
     //display the results section  
     $('#results').removeClass('hidden');
-  }
+};
 
-  
+//watch the form for user action
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
     const searchTerm = $('#js-search-term').val();
-    // getTrails(maxDistance);
     getGeo(searchTerm);   
   });
 
@@ -207,11 +212,11 @@ function watchForm() {
     if ($(window).scrollTop() > $('body').height() / 2) {
     $('.logo-search').removeClass('hidden');
     $('#js-footer').removeClass('hidden');
-    }
+  }
     else if ($('#js-top').focus()){
     $('.logo-search').addClass('hidden');
-    }
-});
-}
+  }
+  });
+};
 
 $(watchForm);
